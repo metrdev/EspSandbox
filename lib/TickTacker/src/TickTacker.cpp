a55f17b8c032eecb4f43741eddd39e4e15b8eee0
metrdev <>
2025-12-29 22:59:43 +0500

    wip

--- lib/TickTacker/src/TickTacker.cpp
+++ lib/TickTacker/src/TickTacker.cpp
@@ -1,23 +1,19 @@
 #include "TickTacker.h"
 
-TickTacker::TickTacker()
-  : _state(false), _interval(1000)
+#include <Arduino.h>
+
+void execute()
 {
+  static bool state = true;
+
+  Serial.println(state ? "Tick" : "Tack");
+  state = !state;
 }
+
+TickTacker::TickTacker()
+{}
 
 void TickTacker::start(uint32_t interval_ms)
 {
-  _interval = interval_ms;
-  _ticker.attach_ms(_interval, TickTacker::_isr, this);
-}
-
-void TickTacker::_isr(TickTacker* instance)
-{
-  if (instance) instance->toggle();
-}
-
-void TickTacker::toggle()
-{
-  _state = !_state;
-  Serial.println(_state ? "Tick" : "Tack");
+  ticker.attach_ms(interval_ms, ::execute);
 }
--- lib/TickTacker/src/TickTacker.h
+++ lib/TickTacker/src/TickTacker.h
@@ -1,16 +1,14 @@
 #pragma once
-#include <Arduino.h>
+
 #include <Ticker.h>
+#include <cstdint>
 
 class TickTacker {
 public:
   TickTacker();
+
   void start(uint32_t interval_ms);
 
 private:
-  static void _isr(TickTacker* instance);
-  void toggle();
-  Ticker _ticker;
-  volatile bool _state;
-  uint32_t _interval;
+  Ticker ticker;
 };
